b8c4d5c986e6ff1cac3f4b577c64b27b23e01dd3
bayarzlac <34569198+bayarzlac@users.noreply.github.com>
2023-12-10 16:32:19 +0800

    updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,139 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!doctype html>
+<html lang="en" dir="ltr">
 
-        <x-validation-errors class="mb-4" />
+<head>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+    <!-- META DATA -->
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Sash – Bootstrap 5  Admin & Dashboard Template">
+    <meta name="author" content="Spruko Technologies Private Limited">
+    <meta name="keywords"
+        content="admin,admin dashboard,admin panel,admin template,bootstrap,clean,dashboard,flat,jquery,modern,responsive,premium admin templates,responsive admin,ui,ui kit.">
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('admin_assets/images/brand/favicon.ico') }}" />
+
+    <!-- TITLE -->
+    <title>Finpack - Admin Panel</title>
+
+    <!-- BOOTSTRAP CSS -->
+    <link id="style" href="{{ asset('admin_assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+
+    <!-- STYLE CSS -->
+    <link href="{{ asset('admin_assets/css/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin_assets/css/dark-style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin_assets/css/transparent-style.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin_assets/css/skin-modes.css') }}" rel="stylesheet" />
+
+    <!--- FONT-ICONS CSS -->
+    <link href="{{ asset('admin_assets/css/icons.css') }}" rel="stylesheet" />
+
+    <!-- COLOR SKIN CSS -->
+    <link id="theme" rel="stylesheet" type="text/css" media="all"
+        href="{{ asset('admin_assets/colors/color1.css') }}" />
+        
+    
+
+</head>
+
+<body class="app sidebar-mini ltr login-img">
+
+    <!-- BACKGROUND-IMAGE -->
+    <div class="">
+
+        <!-- GLOABAL LOADER -->
+        <div id="global-loader">
+            <img src="{{ asset('admin_assets/images/loader.svg') }}" class="loader-img" alt="Loader">
+        </div>
+        <!-- /GLOABAL LOADER -->
+
+        <!-- PAGE -->
+        <div class="page">
+            <div class="">
+                <!-- CONTAINER OPEN -->
+                <div class="col col-login mx-auto mt-7">
+                    <div class="text-center">
+                        <a href="index.html"><img src="{{ asset('admin_assets/images/brand/logo-white.png') }}"
+                                class="header-brand-img" alt=""></a>
+                    </div>
+                </div>
+                <div class="container-login100">
+                    <div class="wrap-login100 p-6">
+                        <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate>
+                            @csrf
+                            <span class="login100-form-title pb-5">
+                                Нэвтрэх
+                            </span>
+                            @if (session('status'))
+                                <div class="mb-4 font-medium text-sm text-green-600">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            <div class="panel panel-primary">
+                                <div class="wrap-input100 validate-input input-group">
+                                    <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                        <i class="zmdi zmdi-email text-muted" aria-hidden="true"></i>
+                                    </a>
+                                    <input class="input100 border-start-0 form-control ms-0" type="email"
+                                        placeholder="Email" name="email" required>
+                                </div>
+                                <div class="wrap-input100 validate-input input-group" id="Password-toggle">
+                                    <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                        <i class="zmdi zmdi-eye text-muted" aria-hidden="true"></i>
+                                    </a>
+                                    <input class="input100 border-start-0 form-control ms-0" type="password"
+                                        placeholder="Password" name="password" required>
+                                </div>
+                                <div class="text-end pt-4">
+                                    <p class="mb-0">
+                                        <a href="forgot-password.html" class="text-primary ms-1">Forgot Password?</a>
+                                    </p>
+                                </div>
+                                <div class="container-login100-form-btn">
+                                    <button type="submit" class="login100-form-btn btn-primary">Нэвтрэх</button>
+                                </div>
+                            
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- CONTAINER CLOSED -->
             </div>
-        @endif
+        </div>
+        <!-- End PAGE -->
+        
+    </div>
+    <!-- BACKGROUND-IMAGE CLOSED -->
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <!-- JQUERY JS -->
+    <script src="{{ asset('admin_assets/js/jquery.min.js') }}"></script>
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
+    <!-- BOOTSTRAP JS -->
+    <script src="{{ asset('admin_assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+    <!-- SHOW PASSWORD JS -->
+    <script src="{{ asset('admin_assets/js/show-password.min.js') }}"></script>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+    <!-- GENERATE OTP JS -->
+    <script src="{{ asset('admin_assets/js/generate-otp.js') }}"></script>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+    <!-- Perfect SCROLLBAR JS-->
+    <script src="{{ asset('admin_assets/plugins/p-scroll/perfect-scrollbar.js') }}"></script>
 
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+    <!-- Color Theme js -->
+    <script src="{{ asset('admin_assets/js/themeColors.js') }}"></script>
+
+    <!-- CUSTOM JS -->
+    <script src="{{ asset('admin_assets/js/custom.js') }}"></script>
+
+    <!-- FORMVALIDATION JS -->
+    <script src="{{ asset('admin_assets/js/form-validation.js') }}"></script>
+
+    <!-- INPUT MASK JS-->
+    <script src="{{ asset('admin_assets/plugins/input-mask/jquery.mask.min.js') }}"></script>
+</body>
+
+</html>
