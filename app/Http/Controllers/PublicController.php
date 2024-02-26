04dedbf74bff6d13bf36b3a90cc05b5c967217ee
bayarzlac <34569198+bayarzlac@users.noreply.github.com>
2024-02-26 19:23:53 +0800

    update

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\MainMenu;
+use App\Models\Posts;
+use App\Models\PostCategories;
 
 class PublicController extends Controller
 {
@@ -14,5 +16,26 @@ class PublicController extends Controller
         $mainmenu = MainMenu::whereNull('parent_id')->orderBy('num')->get();
 
         return view('index', 'mainmenu');
+    }
+
+    public function menuPage($id)
+    {
+        $menuItem = MainMenu::where('id', $id)->first();
+
+        return view('page', compact('menuItem'));
+    }
+
+    public function post($id)
+    {
+        $post = Posts::where('id', $id)->first();
+
+        return view('post', compact('post'));
+    }
+
+    public function postCategory($id)
+    {
+        $category = PostCategories::where('id', $id)->first();
+
+        return view('category', compact('category'));
     }
 }
